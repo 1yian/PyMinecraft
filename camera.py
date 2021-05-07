0ef93a072ca11258d1d6adfaf318de59870688c5
Randy <ldyu999@gmail.com>
2021-05-07 17:05:38 -0500

    camera and some camera related functions added

--- camera.py
+++ camera.py
@@ -2,46 +2,40 @@ import math
 import matrix_pyglm
 import glm
 
+forward = math.tau / 4
+
 class Camera:
-    def __init__(self, shader, width, height):
-        self.width = width
-        self.height = height
+    def __init__(self):
+        self.projectionMatrix = matrix_pyglm.MatrixGLM4D(glm.mat4(1.0))
+        self.modelviewMatrix = matrix_pyglm.MatrixGLM4D(glm.mat4(1.0))
 
-        # model view matrix and projection matrix
-        self.mv_matrix = matrix_pyglm.MatrixGLM4D(glm.mat4(1.0))
-        self.p_matrix = matrix_pyglm.MatrixGLM4D(glm.mat4(1.0))
+        self.currentPosition = [0, 0, -3]
+        self.currentRotation = [forward, 0]
 
-        self.shader = shader
-        self.shader_matrix_location = self.shader.find_uniform(b"matrix")
+        self.movementInput = [0, 0, 0]
 
-        #position and rotation
-        self.position = [0, 0, -3]
-        self.rotation = [math.tau / 4, 0]
+    def changeCameraOrientation(self, dt):
+        distanceUnit = 5 * dt
 
-        #input
-        self.input = [0, 0, 0]
-
-    def update_camera(self, dt):
-        speed = 7
-        multiplier = speed * dt
-
-        self.position[1] += self.input[1] * multiplier
+        self.currentPosition[1] += self.movementInput[1] * distanceUnit
 
         if self.input[0] or self.input[2]:
-            angle = self.rotation[0] + math.atan2(self.input[2], self.input[0]) - math.tau / 4
-            self.position[0] += math.cos(angle) * multiplier
-            self.position[2] += math.sin(angle) * multiplier
-
-    def update_matrices(self):
+            theta = self.currentRotation[0] + math.atan2(self.input[2], self.input[0]) - forward
+            self.currentPosition[0] += math.cos(theta) * distanceUnit
+            self.currentPosition[2] += math.sin(theta) * distanceUnit
+    
+    def updateMVP(self, shader, windowWidth, windowHeight):
         # projection matrix
-        self.p_matrix.alter(glm.perspective(math.radians(90), float(self.width) / self.height, 0.1, 500))
+        self.projectionMatrix.alter(glm.perspective(math.radians(90), float(self.windowWidth) / self.windowHeight, 0.1, 500))
 
-        # model view matrix
-        self.mv_matrix.alter(glm.mat4(1.0))
-        self.mv_matrix.alter(glm.rotate(self.mv_matrix.getGLMMatrix(), -(self.rotation[0] - math.tau / 4), glm.vec3(0, 1, 0)))
-        self.mv_matrix.alter(glm.rotate(self.mv_matrix.getGLMMatrix(), -self.rotation[1], glm.vec3(math.cos(-(self.rotation[0] - math.tau / 4)), 0, math.sin(-(self.rotation[0] - math.tau / 4)))))
-        self.mv_matrix.alter(glm.translate(self.mv_matrix.getGLMMatrix(), glm.vec3(-self.position[0], -self.position[1], self.position[2])))
+        # model-view matrix
+        self.modelviewMatrix.alter(glm.mat4(1.0))
+        self.modelviewMatrix.alter(glm.rotate(self.modelviewMatrix.getGLMMatrix(), -(self.currentRotation[0] - forward), glm.vec3(0, 1, 0)))
+        self.modelviewMatrix.alter(glm.rotate(self.modelviewMatrix.getGLMMatrix(), -self.currentRotation[1], 
+                                    glm.vec3(math.cos(-(self.currentRotation[0] - forward)), 0, math.sin(-(self.currentRotation[0] - forward)))))
+        self.modelviewMatrix.alter(glm.translate(self.modelviewMatrix.getGLMMatrix(), glm.vec3(-self.currentPosition[0], -self.currentPosition[1], self.currentPosition[2])))
 
-        #mvp matrix
-        mvp_matrix = matrix_pyglm.MatrixGLM4D((self.p_matrix.getGLMMatrix()) * (self.mv_matrix.getGLMMatrix()))
-        self.shader.uniform_matrix(self.shader_matrix_location, mvp_matrix)
+        # MVP matrix
+        mvpMatrix = matrix_pyglm.MatrixGLM4D((self.projectionMatrix.getGLMMatrix()) * (self.modelviewMatrix.getGLMMatrix()))
+        # Send MVP matrix to buffer
+        shader.uniform_matrix(shader.find_uniform(b"mvpMatrix"), mvpMatrix)
--- game.py
+++ game.py
@@ -13,11 +13,11 @@ class Window(pyglet.window.Window):
         super().__init__(*args, **kwargs)
         self.set_minimum_size(MIN_SIZE, MIN_SIZE)
 
-        self.camera = camera.Camera(self.shader, self.width, self.height)
+        self.camera = camera.Camera()
 
         # Initialize VAO
         self.VAO = gl.GLuint(0)
-        gl.glGenVertexArrays(byref(self.VAO))
+        gl.glGenVertexArrays(1, byref(self.VAO))
         gl.glBindVertexArray(self.VAO)
 
         gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, gl.GL_FALSE, 0, 0)
@@ -39,19 +39,39 @@ class Window(pyglet.window.Window):
         gl.glBindBuffer(gl.GL_ELEMENT_ARRAY_BUFFER, self.ibo)
         gl.glBufferData(gl.GL_ELEMENT_ARRAY_BUFFER, sizeof(gl.GLuint * len(Cube.INDICES)),
                         (gl.GLuint * len(Cube.INDICES))(*Cube.INDICES), gl.GL_STATIC_DRAW)
+        
+        self.pause = False
+        self.set_exclusive_mouse(True)
 
-        # Initialize model matrix, perspective
-
-    def update(self, t):
-        if not self.mouse_captured:
-            self.camera.input = [0, 0, 0]
-        self.camera.update_camera(t)
+    def update(self, dt):
+        if self.pause:
+            self.camera.movementInput = [0, 0, 0]
+        self.camera.changeCameraOrientation(dt)
 
     def on_draw(self):
+        self.camera.updateMVP(shader, self.width, self.height)
         self.clear()
 
+    def on_resize(self, width, height):
+        print(f"Resize {width} * {height}") # print out window size
+        gl.glViewport(0, 0, width, height)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.mouse_movement_action(dx, dy)
+
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        self.mouse_movement_action(dx, dy)
+
+    def mouse_movement_action(self, dx, dy):
+        if not self.pause:
+            sens = 0.001
+            self.camera.rotation[0] -= sens * dx
+            self.camera.rotation[1] += sens * dy
+
+            forward = math.tau / 4
+            self.camera.rotation[1] = max(min(forward, self.camera.rotation[1]), -forward) # don't want to snap the character's neck
 
 if __name__ == "__main__":
-    window = Window(width=400, height=400, caption='Minecraft')
+    window = Window(width = 400, height = 400, caption = 'Minecraft')
     gl.glClearColor(0.5, 0.7, 1, 1)
     pyglet.app.run()
